33dde85bcf83d72be1cc7df2be3e72a6b0826f09
samyog7901 <samyogkhadka247@gmail.com>
2025-11-21 15:02:36 +0545

    Ram

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -130,8 +130,9 @@ const finalProducts = product
         <section className="w-full bg-white py-20 px-4 sm:px-8" id="featured-products">
           <div className="max-w-7xl mx-auto flex flex-col lg:flex-row gap-8">
             {/* Mobile Sidebar Toggle */}
+            <h3 className="font-bold text-gray-800 dark:text-white">Filter by Category</h3>
             <div className="flex justify-between items-center lg:hidden mb-4">
-              <h3 className="font-bold text-gray-800 dark:text-white">Filter by Category</h3>
+              
               <button
                 onClick={() => setSidebarOpen(!sidebarOpen)}
                 className="px-4 py-2 bg-purple-600 text-white rounded-lg"
